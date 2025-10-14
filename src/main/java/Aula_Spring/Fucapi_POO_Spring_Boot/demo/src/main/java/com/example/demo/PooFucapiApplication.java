5c5df6aa995ff696d499b219561b96bf87255451
Otavio Vidal <otavio.castelani@gmail.com>
2025-10-13 21:13:15 -0400

    Initialize Spring Boot project structure
    Add initial files for a Spring Boot application including Maven wrapper, .gitignore, .gitattributes, pom.xml, main application class, custom annotation, application properties, and a basic test class. This sets up the project for development and builds with Maven.

---
+++ src/main/java/Aula_Spring/Fucapi_POO_Spring_Boot/demo/src/main/java/com/example/demo/PooFucapiApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import Aula_Spring.Fucapi_POO_Spring_Boot.demo.src.main.java.com.example.demo.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooFucapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooFucapiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/Aula_Spring/Fucapi_POO_Spring_Boot/demo/src/main/java/com/example/demo/SpringBootApplication.java
@@ -0,0 +1,4 @@
+package Aula_Spring.Fucapi_POO_Spring_Boot.demo.src.main.java.com.example.demo;
+
+public @interface SpringBootApplication {
+}
